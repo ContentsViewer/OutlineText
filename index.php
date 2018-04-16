3e6665e8c0e2e2e42a413c09001fed42c9dc291f
ContentsViewer <fivetwothreesix@gmail.com>
2018-04-16 20:51:40 +0900

    Release

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="Style.css" />
+</head>
+
+
+<body>
+
+<?php
+    include "OutlineText.php";
+
+    OutlineText::Init();
+
+
+    $text = file_get_contents("Sample.content");
+
+
+    echo "<pre>";
+
+    echo $text;
+
+    echo "</pre>";
+
+
+
+
+    echo OutlineText::Decode($text);
+
+
+?>
+
+</body>
+
+</html>
